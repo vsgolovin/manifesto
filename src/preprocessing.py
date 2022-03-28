c77fcc13bab4f5201c6def579b4beca603125560
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-03-28 21:17:24 +0300

    Process manifestos

---
+++ src/preprocessing.py
@@ -0,0 +1,45 @@
+import os
+import re
+from string import punctuation
+import pandas as pd
+
+
+INPUT_DIR = os.path.join(os.getcwd(), 'data', 'interim')
+OUTPUT_DIR = os.path.join(os.getcwd(), 'data', 'processed')
+
+
+def main():
+    fname = os.path.join(INPUT_DIR, 'all_manifestos.csv')
+    df = pd.read_csv(fname)
+
+    with open(os.path.join(OUTPUT_DIR, 'manifestos.csv'), 'w') as outfile:
+        outfile.write('id_perm,text\n')
+        for _, row in df.iterrows():
+            outfile.write(str(row['id_perm']) + ',')
+            manifesto = process_text(row['text'])
+            manifesto = manifesto.replace('"', '""')
+            outfile.write('"' + manifesto + '"\n')
+
+
+def process_text(s: str) -> str:
+    # make lowercase
+    s = s.lower()
+
+    # remove numbers
+    s = re.sub(r'[0-9]+(?:\.[0-9]+)?', '', s)
+
+    # remove punctuation
+    for ch in punctuation:
+        s = s.replace(ch, '')
+
+    # change encoding to ascii and remove unicode characters
+    # s = s.encode(encoding='ascii', errors='ignore').decode()
+
+    # remove tabs and multiple spaces
+    s = re.sub(r'\s+', ' ', s)  # done in stemmer
+
+    return s
+
+
+if __name__ == '__main__':
+    main()
